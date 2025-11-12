0fe1603b9386c5c40ef15d40f599951ace4e946c
Veronica <vconti@alumnos.exa.unicen.edu.ar>
2025-11-12 00:11:18 -0300

    Arreglo, Lista, Diccionarios y sus recorridos

--- Program.cs
+++ Program.cs
@@ -6,27 +6,79 @@ namespace MiPrimerPrograma
     {
         static void Main(string[] args)
         {
-            // Ingresar Nombre 
-            Console.WriteLine("Por favor, ingrese su nombre completo:");
-            string nombre = Console.ReadLine();
+            /* // Ingresar Nombre 
+             Console.WriteLine("Por favor, ingrese su nombre completo:");
+             string nombre = Console.ReadLine();
 
-            // Morstrar Nombre y Fecha
-            string fechaHoy = DateTime.Now.ToString("dd/MM/yyyy");
-            Console.WriteLine("Nombre: " + nombre);
-            Console.WriteLine("Fecha de hoy: " + fechaHoy);
-            Console.WriteLine("----------------------------------");
+             // Morstrar Nombre y Fecha
+             string fechaHoy = DateTime.Now.ToString("dd/MM/yyyy");
+             Console.WriteLine("Nombre: " + nombre);
+             Console.WriteLine("Fecha de hoy: " + fechaHoy);
+             Console.WriteLine("----------------------------------");
 
-            Console.WriteLine("Ingrese el primer número:");
-            string entrada1 = Console.ReadLine();
-            int numero1 = int.Parse(entrada1);
+             Console.WriteLine("Ingrese el primer número:");
+             string entrada1 = Console.ReadLine();
+             int numero1 = int.Parse(entrada1);
 
-            Console.WriteLine("Ingrese el segundo número:");
-            string entrada2 = Console.ReadLine();
-            int numero2 = int.Parse(entrada2);
+             Console.WriteLine("Ingrese el segundo número:");
+             string entrada2 = Console.ReadLine();
+             int numero2 = int.Parse(entrada2);
 
-            // Muestra suma para segundo commit
-            int suma = numero1 + numero2;
-            Console.WriteLine("La suma de los dos números es: " + suma);
+             // Muestra suma para segundo commit
+             int suma = numero1 + numero2;
+            Console.WriteLine("La suma de los dos números es: " + suma); */
+
+            //Arreglo estatico de tamaño fijo
+            /*
+            string[] frutas = new string[3];
+            frutas[0] = "Manzana";
+            frutas[1] = "Banana";
+            frutas[2] = "Naranja"; */
+
+            //Una forma de recorrer el array
+            /* for (int i = 0; i < frutas.Length; i++)
+             {
+                 Console.WriteLine("Fruta " + (i + 1) + ": " + frutas[i]);
+             } */
+
+            //Otra forma de recorrer el array
+            /*
+            foreach (string fruta in frutas)
+            {
+                Console.WriteLine("Arreglo: " + fruta);
+            }
+
+            //Lista de tamaño dinamico
+            List<string> frutas = new List<string>();
+            frutas.Add("Manzana");
+            frutas.Add("Banana");
+            frutas.Add("Naranja");
+
+            foreach (string fruta in frutas)
+            {
+                Console.WriteLine("Fruta: " + fruta);
+
+            } 
+
+            //Diccionario mas rapido que la Lista
+            Dictionary<int, string> frutasDicc = new Dictionary<int, string>();
+
+            frutasDicc.Add(1, "Kiwi");
+            frutasDicc.Add(2, "Sandia");
+            frutasDicc.Add(3, "Frutilla"); */
+
+            Dictionary<string, int> frutasDicc = new Dictionary<String, int>();
+
+            frutasDicc.Add("Kiwi", 10);
+            frutasDicc.Add("Sandia", 99);
+            frutasDicc.Add("Frutilla", 100);
+
+            foreach (KeyValuePair<string, int> fruta in frutasDicc)
+            {
+                Console.WriteLine("Clave: " + fruta.Key + " - Fruta: " + fruta.Value);
+            }
+
+
         }
     }
 }
--- obj/Debug/net8.0/HolaMundo.AssemblyInfo.cs
+++ obj/Debug/net8.0/HolaMundo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729aa48dd104dad4d1f9d74f59116c7f6de05819")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
