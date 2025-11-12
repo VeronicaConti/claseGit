ae38de66a668f1629dff53c2af5887f768edbab5
Veronica <vconti@alumnos.exa.unicen.edu.ar>
2025-11-11 22:58:20 -0300

    Primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MiPrimerPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pide Nombre 
+            Console.WriteLine("Por favor, ingrese su nombre completo:");
+            string nombre = Console.ReadLine();
+
+            // Muestra nombre y fechas
+            string fechaHoy = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Fecha de hoy: " + fechaHoy);
+            Console.WriteLine("----------------------------------");
+
+            // Solicitar dos números
+            Console.WriteLine("Ingrese el primer número:");
+            string entrada1 = Console.ReadLine();
+            int numero1 = int.Parse(entrada1);
+
+            Console.WriteLine("Ingrese el segundo número:");
+            string entrada2 = Console.ReadLine();
+            int numero2 = int.Parse(entrada2);
+
+            // Calcular y mostrar la suma
+            int suma = numero1 + numero2;
+            Console.WriteLine("La suma de los dos números es: " + suma);
+        }
+    }
+}
